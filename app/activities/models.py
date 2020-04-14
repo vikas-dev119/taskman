b98f31669b71a756c4d41778cf85fa9efd82e3c2
Vikas shrivastava <vikas.shrivastava@jiffyship.in>
2020-04-14 21:59:15 +0530

    update

--- app/activities/models.py
+++ app/activities/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.utils import timezone
 from user_manager.models import User
 
 
 class Activity(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_activity", default=None)
     message = models.CharField(default=None, max_length=50)
-    created_on = models.DateTimeField(default=timezone.now)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'activity'
--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-from datetime import datetime
-from django.utils import timezone 
 from user_manager.models import User
 
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_task", default=None)
     name = models.CharField(max_length=50, default=None, blank=False, null=False)
-    deadline = models.DateTimeField(default=datetime.now())
+    deadline = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(default=0, blank=True, null=True, help_text='1->Active, 0->Inactive' )
-    created_on = models.DateTimeField(default=datetime.now(), blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'task'
